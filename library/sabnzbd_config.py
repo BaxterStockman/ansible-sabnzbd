1c74dd9d32aeed68027038b107d5238d3af4a729
Matt Schreiber <EMAIL>
2015-09-22 08:29:33 -0400

    Updated documentation of sabznbd_config.py and started conformation to Ansible modules best practices

--- library/sabnzbd_config.py
+++ library/sabnzbd_config.py
@@ -1,79 +1,96 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# (c) 2012, Jan-Piet Mens <jpmens () gmail.com>
+# (c) 2015, Matt Schreiber <schreibah () gmail.com>,
+# <https://github.com/BaxterStockman>
 #
-# This file is part of Ansible
+# Based substantially on ini_file.py, (c) 2012, Jan-Piet Mens
+# <jpmens () gmail.com>.
 #
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Released under the same terms as Ansible itself, as reproduced below:
 #
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#   'Ansible is free software: you can redistribute it and/or modify it under
+#   the terms of the GNU General Public License as published by the Free
+#   Software Foundation, either version 3 of the License, or (at your option)
+#   any later version.
 #
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#   You should have received a copy of the GNU General Public License along
+#   with Ansible.  If not, see <http://www.gnu.org/licenses/>.'
 #
 
 DOCUMENTATION = '''
 ---
-module: ini_file
-short_description: Tweak settings in INI files
+module: sabnzbd_config
+short_description: Manage C(sabnzbd.ini)
 extends_documentation_fragment: files
 description:
-     - Manage (add, remove, change) individual settings in an INI-style file without having
-       to manage the file as a whole with, say, M(template) or M(assemble). Adds missing
-       sections if they don't exist.
-     - Comments are discarded when the source file is read, and therefore will not
-       show up in the destination file.
-version_added: "0.9"
+     - Add, remove or update settings in the SABnzbd configuration file
+       C(sabnzbd.ini) without needing to manage the whole shebang.
+     - Supports a I(batch) mode for making multiple configuration changes in
+       one go.
+version_added: "1.9"
 options:
   dest:
     description:
-      - Path to the INI-style file; this file is created if required
-    required: true
-    default: null
+      - Path to C(sabnzbd.ini).
+    required: no
+    default: C(/opt/sabnzbd/sabnzbd.ini)
+  libdir:
+    description:
+      - Path to the SABnzbd Python libraries.
+    required: no
+    default: C(os.path.basename(/opt/sabnzbd))
   section:
     description:
-      - Section name in INI file. This is added if C(state=present) automatically when
-        a single value is being set.
-    required: true
+      - The name of a C([section]) in C(sabnzbd.ini).
+      - Will be ignored in batch mode.
+    required: no
     default: null
   option:
     description:
-      - if set (required for changing a I(value)), this is the name of the option.
-      - May be omitted if adding/removing a whole I(section).
-    required: false
+      - The left side of an C(option = value) pair.
+      - Will be ignored in batch mode and when deleting a whole section.
+    required: no
     default: null
   value:
     description:
-     - the string value to be associated with an I(option). May be omitted when removing an I(option).
-    required: false
+      - The right side of an C(option = value) pair
+      - Will be ignored in batch mode and when deleting a whole section.
+    required: no
     default: null
-  backup:
+  settings:
     description:
-      - Create a backup file including the timestamp information so you can get
-        the original file back if you somehow clobbered it incorrectly.
-    required: false
+      - A hash of settings that will be merged into C(sabnzbd.ini).
+      - Top-level keys are section names, (almost) everything else are
+        C(option = value) pairs (see the 'Notes' section for the exception).
+  state:
+    description:
+      - The desired state of the section, option, or hash of settings.
+    required: no
     default: "no"
     choices: [ "yes", "no" ]
-  others:
-     description:
-       - all arguments accepted by the M(file) module also work here
-     required: false
+  backup:
+    description:
+      - Create a timestamped backup of the original C(sabnzbd.ini).
+    required: no
+    default: "no"
+    choices: [ "yes", "no" ]
 notes:
-   - While it is possible to add an I(option) without specifying a I(value), this makes
-     no sense.
-   - A section named C(default) cannot be added by the module, but if it exists, individual
-     options within the section can be updated. (This is a limitation of Python's I(ConfigParser).)
-     Either use M(template) to create a base INI file with a C([default]) section, or use
-     M(lineinfile) to add the missing line.
-requirements: [ ConfigParser ]
-author: Jan-Piet Mens
+    - This module uses SABnzbd's library code for manipulating the
+      configuration file, so SABnzbd must be installed and its libraries must
+      be readable or the module will fail to run.
+    - If C(sabnzbd.ini) does not already exist, it will be created.
+    - SABnzbd recognizes some nested settings, e.g. C(categories), which can
+      contain arbitrary sub-categories, such as C([[movies]]), C([[tv]]), and
+      so on.  By using SABnzbd's own internal code for manipulating the
+      configuration file, this module should just Do The Right Thing.  However,
+      it is up to the caller to ensure, for example, that I(section) and
+      I(option) names are valid; no validation is performed by the module.
+
+requirements: [ sabnzbd ]
+
+author: Matt Schreiber U(https://github.com/BaxterStockman), based on Jan-Piet
+        Mens' work on the M(ini_file) module.
 '''
 
 EXAMPLES = '''
@@ -90,17 +107,16 @@ EXAMPLES = '''
 import sys
 
 # ==============================================================
-# do_config
-
+# SABnzbdConfigWrapper
 
 class SABnzbdConfigWrapper:
     def __init__(self, module, filename, state='batch', libdir=None,
-                 options=None, section=None, option=None, value=None,
+                 settings=None, section=None, option=None, value=None,
                  backup=False):
         self.module = module
         self.filename = filename
         self.state = state
-        self.options = options
+        self.settings = settings
         self.option = option
         self.value = value
         self.section = section
@@ -109,15 +125,14 @@ class SABnzbdConfigWrapper:
         self.set_operation()
         self.set_libdir(libdir)
 
-        libdir = self.libdir
         try:
-            sys.path.append(libdir)
+            sys.path.append(self.libdir)
             import sabnzbd.config
-        except ImportError:
+        except:
             self.module.fail_json(msg="Can't load SABnzbd python libraries from %s"
                                   % libdir)
         else:
-            self.sabnzbd_config = sabnzbd.config
+            self.sabconfig = sabnzbd.config
 
     def set_operation(self, state=None):
         if state is None:
@@ -129,9 +144,6 @@ class SABnzbdConfigWrapper:
             self.operation = self.do_present
         elif state == 'absent':
             self.operation = self.do_absent
-        else:
-            self.module.fail_json(msg=("%s is not a valid value for state" %
-                                       state))
 
     def set_libdir(self, libdir=None):
         if libdir is None:
@@ -139,70 +151,111 @@ class SABnzbdConfigWrapper:
             libdir = os.path.dirname(self.filename)
         self.libdir = libdir
 
+    def validate(self):
+        if state == 'batch':
+            if not isinstance(self.settings, dict):
+                self.module.fail_json(msg="must provide a hash of settings when state=batch")
+        elif state == 'present':
+            self.operation = self.do_present
+        elif state == 'absent':
+            self.operation = self.do_absent
+        else:
+            self.module.fail_json(msg=("%s is not a valid state" % self.state))
+
     def run(self, *args, **kwargs):
-        self.load_config
-        failmsg = '%s' % self.sabnzbd_config.CFG
-        self.module.fail_json(msg=failmsg)
-        changed = self.operation(*args, **kwargs)
+        # Load sabnzbd.ini
+        self.load_config()
+
+        # Execute the operation
+        self.operation(*args, **kwargs)
+
+        # Determine whether a change occurred
+        changed = self.is_changed()
+
+        self.module.fail_json(msg=("changed: %s" % changed))
+
+        # Write out the configuration
         self.save_config(changed)
+
         return changed
 
-    def load_config(self):
+    def load_config(self, filename=None):
+        module = self.module
+
+        if filename is None:
+            filename = self.filename
+
         read_res = True
         read_msg = ''
         try:
-            read_res, read_msg = self.sabnzbd_config.read_config(self.filename)
+            read_res, read_msg = self.sabconfig.read_config(filename)
         except:
             module.fail_json(msg="Can't read SABnzbd config file %s: %s" % (
                 filename, sys.exc_info()[0]
             ))
 
         if not read_res:
-            module.fail_json(msg="Can't read SABnzbd config file %s: %s" % ( filename, read_msg))
+            module.fail_json(msg="Can't read SABnzbd config file %s: %s" %
+                             (filename, read_msg))
 
-        # Merge the internal options database into the object representing the INI
-        # file
+        # Merge the internal settings database into the object representing the
+        # INI file
         got_dconfig = True
         dconfig = None
         try:
-            (got_dconfig, dconfig) = self.sabnzbd_config.get_dconfig(None, None)
+            (got_dconfig, dconfig) = self.sabconfig.get_dconfig(None, None)
         except:
-            module.fail_json(msg="Can't load SABnzbd database object %s: %s" % ( filename, sys.exc_info()[0]))
+            module.fail_json(msg="Can't load SABnzbd database object %s: %s" %
+                             (filename, sys.exc_info()[0]))
 
         if not got_dconfig:
-            module.fail_json(
-                msg="Can't load SABnzbd database object %s: %s" % filename
-            )
+            module.fail_json(msg="Can't load SABnzbd database object %s: %s" %
+                             filename)
 
-        # Save the initial configuration.
-        self.sabnzbd_config.CFG.merge(dconfig)
-        self.orig_config = self.sabnzbd_config.CFG
+        # Save the initial configuration.  Deep copy to avoid
+        # action-at-a-distance.
+        self.sabconfig.CFG.merge(dconfig)
+        #self.orig_config = self.sabconfig.CFG.copy()
+        self.orig_config = self.sabconfig.CFG.dict()
 
-    def save_config(self, changed):
+    def save_config(self, changed=None):
         module = self.module
-        if changed is None:
-            changed = self.changed
+        filename = self.filename
 
-        self.sabnzbd_config.modified = changed
+        if changed is None:
+            changed = self.is_changed()
+
+        # Do this or the .modified method will exit immediately
+        self.sabconfig.modified = changed
 
         write_res = False
         if changed and not module.check_mode:
-            if backup:
+            if self.backup:
                 module.backup_local(filename)
-
             try:
-                write_res = self.sabnzbd_config.CFG.write()
+                self.sabconfig.CFG.write()
             except:
-                module.fail_json(msg="Can't save SABnzbd config file %s: %s" % (
-                    filename, sys.exc_info()[0]
-                ))
+                module.fail_json(msg="Can't save SABnzbd config file %s: %s" %
+                                 (filename, sys.exc_info()[0]))
 
-        return write_res
+    def is_changed(self):
+        if self.orig_config is None:
+            return False
+
+        self.module.fail_json(msg=("orig: %s\nnew: %s") % (self.orig_config, self.sabconfig.CFG.dict()))
+
+        return (cmp(self.orig_config, self.sabconfig.CFG.dict()) != 0)
+
+    def do_present(self):
+        self.sabconfig.CFG.sections.append(self.section)
+        self.sabconfig.CFG.update({self.section: self.value})
+
+    def do_absent(self):
+        pass
 
     def do_batch(self):
         # Now merge the wanted settings and compare
-        self.sabnzbd_config.CFG.merge(options)
-        return cmp(orig_config, self.sabnzbd_config.CFG) != 0
+        self.sabconfig.CFG.merge(self.settings)
 
 
 # ==============================================================
@@ -211,14 +264,14 @@ class SABnzbdConfigWrapper:
 def main():
     module = AnsibleModule(
         argument_spec = dict(
-            dest = dict(required=True),
-            libdir = dict(required=False),
-            options = dict(required=True),
-            section = dict(required=False),
-            option = dict(required=False),
-            value = dict(required=False),
-            backup = dict(default='no', type='bool'),
-            state = dict(default='batch')
+            dest     = dict(required=True),
+            libdir   = dict(required=False),
+            settings = dict(required=True),
+            section  = dict(required=False),
+            option   = dict(required=False),
+            value    = dict(required=False),
+            backup   = dict(default='no', type='bool'),
+            state    = dict(default='batch')
         ),
         add_file_common_args = True,
         supports_check_mode = True
@@ -231,18 +284,18 @@ def main():
     section = module.params['section']
     option = module.params['option']
     value = module.params['value']
-    options = module.params['options']
+    settings = module.params['settings']
     state = module.params['state']
     backup = module.params['backup']
 
     config = SABnzbdConfigWrapper(module, dest, libdir=libdir, section=section,
-                                  option=option, value=value, options=options,
-                                  state=state, backup=backup)
+                                  option=option, value=value,
+                                  settings=settings, state=state,
+                                  backup=backup)
 
     changed = config.run()
 
     file_args = module.load_file_common_arguments(module.params)
-    changed = module.set_fs_attributes_if_different(file_args, changed)
     changed = module.set_fs_attributes_if_different(file_args, changed)
 
     # Mission complete
@@ -250,4 +303,6 @@ def main():
 
 # import module snippets
 from ansible.module_utils.basic import *
-main()
+
+if __name__ == '__main__':
+    main()
